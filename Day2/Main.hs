e85b65a9aad4674835833927fc37e7f81c0a9b2d
BradHesson <55291084+Brad-Hesson@users.noreply.github.com>
2021-12-03 22:22:39 +0000

    Solved Day 2

---
+++ Day2/Main.hs
@@ -0,0 +1,33 @@
+module Day2.Main where
+
+import Data.Char
+import Data.List
+import Control.Arrow
+
+data Move = F Int | D Int | U Int
+  deriving (Show)
+
+movesFromFile :: String -> IO [Move]
+movesFromFile path = return . map (f . partition isNumber) . lines =<< readFile path
+  where
+    f (n,"forward ") = F $ read n
+    f (n,"up ") = U $ read n
+    f (n,"down ") = D $ read n
+
+part1 (h,v) (F n) = (h+n,v)
+part1 (h,v) (U n) = (h,v-n)
+part1 (h,v) (D n) = (h,v+n)
+
+part2 (h,v,a) (F n) = (h+n, v+n*a, a)
+part2 (h,v,a) (U n) = (h, v, a-n)
+part2 (h,v,a) (D n) = (h, v, a+n)
+
+main :: IO ()
+main = do
+  moves <- movesFromFile "Day2/input.txt"
+
+  putStr "Part 1: "
+  print . (\(h,v) -> h*v) $ foldl part1 (0,0) moves
+
+  putStr "Part 2: "
+  print . (\(h,v,a) -> h*v) $ foldl part2 (0,0,0) moves
--- Main.hs
+++ Main.hs
@@ -1,3 +1,11 @@
 import qualified Day1.Main as D1
+import qualified Day2.Main as D2
 
-main = D1.main
+main = do
+  putStrLn "[ Day 1 ]"
+  D1.main
+  putStrLn ""
+  
+  putStrLn "[ Day 2 ]"
+  D2.main
+  return ()
