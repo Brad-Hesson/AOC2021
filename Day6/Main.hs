d26ec3117527a172d4d09e71d5e984f43998e4ab
Brad Hesson <brad.hesson@outlook.com>
2021-12-06 16:19:36 -0700

    improved readability

--- Day6/Main.hs
+++ Day6/Main.hs
@@ -11,13 +11,7 @@ splitBy :: (a -> Bool) -> [a] -> [[a]]
 splitBy f = filter (not . f . head) . groupBy (curry $ uncurry (==) . (f***f))
 
 fishFromFile :: FilePath -> IO FishGen
-fishFromFile = return
-  . map ((flip (-) 1) . length)
-  . group
-  . sort
-  . (++) [0..8]
-  . map read
-  . splitBy (==',') <=< readFile
+fishFromFile = return . map ((flip (-) 1) . length) . group . sort . (++) [0..8] . map read . splitBy (==',') <=< readFile
 
 rotateLeft1 :: [a] -> [a]
 rotateLeft1 = liftM2 (++) tail (singleton . head)
@@ -25,11 +19,14 @@ rotateLeft1 = liftM2 (++) tail (singleton . head)
 nextGen :: FishGen -> FishGen
 nextGen = uncurry (++) . second (\[a6,a7,a8]-> [a6+a8,a7,a8]) . splitAt 6 . rotateLeft1
 
+genPop :: Int -> FishGen -> Int
+genPop n = sum . head . drop n . iterate nextGen
+
 part1 :: [Int] -> Int
-part1 = sum . head . drop 80 . iterate nextGen
+part1 = genPop 80
 
 part2 :: [Int] -> Int
-part2 = sum . head . drop 256 . iterate nextGen
+part2 = genPop 256
 
 main = do
   fish <- fishFromFile "Day6/input.txt"
