49a07c5a4cd195f588b9bf0fafa354edee58678f
Brad Hesson <brad.hesson@outlook.com>
2021-12-30 19:38:01 -0700

    changed from array to map

--- Day15/Main.hs
+++ Day15/Main.hs
@@ -1,20 +1,12 @@
 module Day15.Main where
 
-import Control.Arrow (Arrow ((&&&), (***)))
-import Data.Array
-  ( Array,
-    Ix (inRange),
-    array,
-    bounds,
-    listArray,
-    (!),
-    (//),
-  )
+import Control.Arrow (Arrow (second, (&&&), (***)))
+import Data.Array (Array, array, assocs, bounds, listArray, (!))
 import Data.Char (digitToInt, isNumber)
 import Data.List (sortOn)
 import qualified Data.Map as M
 
-type Maze = Array Point Node
+type Maze = M.Map Point Node
 
 type Point = (Int, Int)
 
@@ -28,33 +20,33 @@ arrayFromFile fp = do
   return . listArray ((0, 0), (rs - 1, cs - 1)) . map digitToInt . filter isNumber $ raw
 
 mazeFromArray :: Array (Int, Int) Int -> Maze
-mazeFromArray = fmap (Node False)
+mazeFromArray = M.fromList . map (second $ Node False) . assocs
 
 adjacents :: Maze -> Point -> [Point]
 adjacents mz =
   let offsets (xi, yi) = [(x + xi, y + yi) | (x, y) <- [(1, 0), (-1, 0), (0, 1), (0, -1)]]
-   in filter (inRange $ bounds mz) . offsets
+   in filter (`M.member` mz) . offsets
 
-setVisited :: [Point] -> Maze -> Maze
-setVisited ps mz =
+setVisited :: Point -> Maze -> Maze
+setVisited p mz =
   let setVisitedOne (Node _ n) = Node True n
-   in mz // zip ps (map (setVisitedOne . (mz !)) ps)
+   in M.adjust setVisitedOne p mz
 
 dijkstra :: Point -> Maze -> [(Point, Int)] -> [(Point, Int)]
 dijkstra endP mz (pqh : pqrest)
   | endP == fst pqh = [pqh]
-  | isVisited . (mz !) . fst $ pqh = dijkstra endP mz pqrest
-  | all (isVisited . (mz !)) . adjacents mz . fst $ pqh = dijkstra endP mz pqrest
+  | isVisited . (mz M.!) . fst $ pqh = dijkstra endP mz pqrest
+  | all (isVisited . (mz M.!)) . adjacents mz . fst $ pqh = dijkstra endP mz pqrest
   | otherwise =
-    let mz' = setVisited [fst pqh] mz
+    let mz' = setVisited (fst pqh) mz
         adjs = adjacents mz . fst $ pqh
-        fltadjs = filter (not . all (isVisited . (mz' !)) . adjacents mz) . filter (not . isVisited . (mz' !)) $ adjs
-        pqadjs = map (id &&& (+ snd pqh) . getCost . (mz !)) fltadjs
+        fltadjs = filter (not . all (isVisited . (mz' M.!)) . adjacents mz) . filter (not . isVisited . (mz' M.!)) $ adjs
+        pqadjs = map (id &&& (+ snd pqh) . getCost . (mz M.!)) fltadjs
         pq' = sortOn snd $ pqadjs ++ pqrest
      in pqh : dijkstra endP mz' pq'
 
 part1 :: Array (Int, Int) Int -> Int
-part1 arr = snd . last . dijkstra (snd . bounds . mazeFromArray $ arr) (mazeFromArray arr) $ [((0, 0), 0)]
+part1 arr = snd . last . dijkstra (snd . bounds $ arr) (mazeFromArray arr) $ [((0, 0), 0)]
 
 part2 :: Array (Int, Int) Int -> Int
 part2 arr =
@@ -62,7 +54,7 @@ part2 arr =
       f x y = (\x -> if x > 9 then x - 9 else x) . (+ (y `div` sy)) . (+ (x `div` sx)) . (arr !) $ (x `mod` sx, y `mod` sy)
       lst = [((x, y), f x y) | x <- [0 .. sx * 5 - 1], y <- [0 .. sy * 5 - 1]]
       mz = mazeFromArray . array ((0, 0), (sx * 5 - 1, sy * 5 - 1)) $ lst
-      endPoint = snd . bounds $ mz
+      endPoint = (sx * 5 - 1, sy * 5 - 1)
       soln = dijkstra endPoint mz [((0, 0), 0)]
    in snd . last $ soln
 
